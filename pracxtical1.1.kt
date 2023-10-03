a0b7a43bf937aa5374bd92ae4a5da55e49907306
PATEL JAY BABUBHAI <jaybabulalpatel21@gnu.ac.in>
2023-10-03 21:21:13 +0530

    Initial commit

---
+++ pracxtical1.1.kt
@@ -0,0 +1,40 @@
+fun main(){
+    var a:Int=22
+    val a1:Int=25
+    var b:Float=1.5F
+    val b1:Float=1.7F
+    var c:Char='T'
+    val c1:Char='A'
+    var d:String="HMS"
+    val d1:String="jjp"
+    var e:Boolean=false
+    val e1:Boolean=true
+    var f:Double=96.36
+    val f1:Double=90.36
+    var g:Long=338927847
+    val g1:Long=231236512
+    var h:Short=-2
+    val h1:Short=-5
+    var i:Byte=127
+    val i1:Byte=125
+
+
+    println("Integer Value:"+a)
+    println("const Integer Value:"+a1)
+    println("Float Value:"+b)
+    println("const Float Value:"+b1)
+    println("Char Value:"+c)
+    println("const Char Value:"+c1)
+    println("String Value:"+d)
+    println("const String Value:"+d1)
+    println("Boolean Value:"+e)
+    println("const Boolean Value:"+e1)
+    println("Double Value:"+f)
+    println("const Double Value:"+f1)
+    println("Long Value:"+g)
+    println("const Long Value:"+g1)
+    println("Short Value:"+h)
+    println("const Short Value:"+h1)
+    println("Byte Value:"+i)
+    println("const Byte Value:"+i1)
+}
